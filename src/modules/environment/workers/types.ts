5f44624c5244ff6ceab0088422b16d88cfbd58d3
AnthemFlynn <AnthemFlynn@users.noreply.github.com>
2025-12-12 10:40:08 -0800

    feat: add timing instrumentation to LightingWorker

--- src/modules/environment/workers/types.ts
+++ src/modules/environment/workers/types.ts
@@ -6,12 +6,13 @@ export type ChunkRequest =
       neighborVoxels: Record<string, ArrayBuffer> // Now contains Blocks AND Light
     }
 
-export type ChunkResponse = 
+export type ChunkResponse =
   | {
       type: 'LIGHT_CALCULATED'
       x: number
       z: number
       chunkBuffer: ArrayBuffer // Updated buffer with light
+      timingMs: number
     }
 
 export type WorkerMessage = ChunkRequest
