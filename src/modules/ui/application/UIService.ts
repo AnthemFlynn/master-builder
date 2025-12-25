52c2cc7d971c6812578fbf767a0835a2dc865514
AnthemFlynn <AnthemFlynn@users.noreply.github.com>
2025-12-24 19:38:45 -0800

    feat(ui): connect SaveLoadModal to menu buttons

--- src/modules/ui/application/UIService.ts
+++ src/modules/ui/application/UIService.ts
@@ -1,14 +1,19 @@
 import { EventBus } from '../../game/infrastructure/EventBus'
+import { CommandBus } from '../../game/infrastructure/CommandBus'
 import { UIState } from '../domain/UIState'
 import { IUIQuery } from '../ports/IUIQuery'
 import { HUDManager } from './HUDManager'
 import { MenuManager } from './MenuManager'
 import { RadialMenuManager } from './components/RadialMenuManager'
 import { CreativeModalManager } from './components/CreativeModalManager'
+import { SaveLoadModal } from '../components/SaveLoadModal'
 import { InventoryService } from '../../inventory/application/InventoryService'
 import { InventoryBank } from '../../inventory/domain/InventoryState'
 import { DebugOverlay } from './DebugOverlay'
 import { PerformanceMonitor } from '../../game/infrastructure/PerformanceMonitor'
+import { IPersistenceQuery } from '../../persistence/ports/IPersistenceQuery'
+import { SaveGameCommand } from '../../persistence/domain/commands/SaveGameCommand'
+import { LoadGameCommand } from '../../persistence/domain/commands/LoadGameCommand'
 
 interface Position {
   x: number
@@ -28,6 +33,7 @@ export class UIService implements IUIQuery {
   private menuManager: MenuManager
   private radialMenuManager: RadialMenuManager
   private creativeModalManager: CreativeModalManager
+  private saveLoadModal: SaveLoadModal | null = null
   private debugOverlay: DebugOverlay
 
   constructor(
@@ -56,7 +62,7 @@ export class UIService implements IUIQuery {
         exitPointerLock: this.options.exitPointerLock
       }
     )
-    
+
     this.radialMenuManager = new RadialMenuManager(inventory)
     this.creativeModalManager = new CreativeModalManager(inventory, () => {
         // When modal closes itself, return to playing
@@ -64,6 +70,9 @@ export class UIService implements IUIQuery {
     })
 
     this.debugOverlay = new DebugOverlay(performanceMonitor, options.getPlayerPosition)
+
+    // Wire up the "Load Game" button (modal will be set later)
+    this.setupSaveLoadButton()
 
     // Listen for mouse movements for the radial menu
     this.eventBus.on('input', 'InputMouseMoveEvent', (e: any) => {
@@ -74,6 +83,37 @@ export class UIService implements IUIQuery {
 
     // Start in menu state (HTML shows menu by default)
     this.setState(UIState.MENU)
+  }
+
+  private setupSaveLoadButton(): void {
+    const saveButton = document.querySelector('#save')
+    saveButton?.addEventListener('click', () => {
+      this.openSaveLoadModal()
+    })
+  }
+
+  openSaveLoadModal(): void {
+    if (this.saveLoadModal) {
+      this.saveLoadModal.open()
+    }
+  }
+
+  /**
+   * Set up persistence for save/load modal (called after persistence is initialized)
+   */
+  setPersistence(commandBus: CommandBus, persistenceQuery: IPersistenceQuery): void {
+    this.saveLoadModal = new SaveLoadModal({
+      onSave: async (slotId) => {
+        commandBus.send(new SaveGameCommand(slotId, slotId, false))
+      },
+      onLoad: async (slotId) => {
+        commandBus.send(new LoadGameCommand(slotId))
+      },
+      onClose: () => {
+        // Return to previous state (menu or pause)
+      },
+      listSlots: () => persistenceQuery.listSaveSlots()
+    })
   }
 
   setState(newState: UIState): void {
